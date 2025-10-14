34c103d7abfc26b452863a1b19b132a968519679
RioluMeltan <78819159+RioluMeltan@users.noreply.github.com>
2025-10-14 05:19:12 +0000

    Missed something

--- streamlit_app.py
+++ streamlit_app.py
@@ -43,10 +43,6 @@ for stock in streamlit.session_state.tracked_stocks:
     data = yfinance.download(stock, start = datetime.datetime.now() - datetime.timedelta(days = {'Previous 5 Days': 5, 'Previous Month': 30, 'Previous 6 Months': 180}[selected_range]), end = datetime.datetime.now(), interval = '1d', progress = False, auto_adjust = True)
     
     fig = plotly.graph_objs.Figure()
-    if not data.empty:
-        streamlit.line_chart(data['Close'])
-    else:
-        streamlit.warning(f"No data available for {stock}")
     fig.add_trace(plotly.graph_objs.Scatter(x = data.index, y = data['Close'], mod = 'lines', name = stock, line = dict(color = 'blue', width = 2, dash = 'solid')))
     fig.update_layout(title = f'{stock} Closing Price', xaxis_title = 'Date', yaxis_title = "Price (USD)", template = 'plotly_dark', showlegend = True, height = 400)
     streamlit.plotly_chart(fig, use_container_width = True)
