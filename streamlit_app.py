e1f4b55c2fc238769b7b674aaa0822e81faae360
RioluMeltan <78819159+RioluMeltan@users.noreply.github.com>
2025-10-14 04:16:16 +0000

    Ticker check

--- streamlit_app.py
+++ streamlit_app.py
@@ -13,16 +13,20 @@ if 'tracked_stocks' not in streamlit.session_state:
 
 streamlit.set_page_config(page_title = 'CrocStocks Stock Predictor', layout = 'wide')
 streamlit.sidebar.header('Add Stocks to Watchlist')
-ticker = streamlit.sidebar.text_input('Enter Stock Ticker', value = 'AAPL')
-start_date = streamlit.sidebar.date_input('Start Date', value = datetime.datetime.now() - datetime.timedelta(days = 365))
-end_date = streamlit.sidebar.date_input('End Date', value = datetime.datetime.now())
-if streamlit.sidebar.button('Add to Watchlist'):
-    if ticker.upper() not in streamlit.session_state.tracked_stocks:
+ticker = streamlit.sidebar.text_input('Enter Stock Ticker(default AAPL)', value = 'AAPL')
+start_date = streamlit.sidebar.date_input('Start Date(default one year)', value = datetime.datetime.now() - datetime.timedelta(days = 365))
+end_date = streamlit.sidebar.date_input('End Date(default one year)', value = datetime.datetime.now())
+
+if streamlit.sidebar.button('Add to Watchlist'): 
+    if len(ticker.upper().strip()) == 0: 
+        streamlit.sidebar.error('Please enter a valid stock ticker.')
+    elif ticker.upper() in streamlit.session_state.tracked_stocks: 
+        streamlit.sidebar.warning(f"{ticker.upper()} is already in your watchlist.")
+    elif ticker.upper() not in streamlit.session_state.tracked_stocks: 
         streamlit.session_state.tracked_stocks.append(ticker.upper())
         streamlit.sidebar.success(f'You have added {ticker.upper()} to your watchlist!')
-    elif ticker.upper() in streamlit.session_state.tracked_stocks:
-        streamlit.sidebar.warning(f"{ticker.upper()} is already in your watchlist.")
 
 streamlit.sidebar.subheader('Your Watchlist')
-for stock in streamlit.session_state.tracked_stocks:
+for stock in streamlit.session_state.tracked_stocks: 
+    close_diff = yfinance.download(stock, start = datetime.datetime.now() - datetime.timedelta(days = 1), end = datetime.datetime.now(), interval = '1d', progress = False, auto_adjust = True)
     streamlit.sidebar.write(f'{stock}')
