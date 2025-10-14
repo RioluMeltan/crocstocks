4101a8295f9e5f7345dfd0ed2cc1da21d82d332f
RioluMeltan <78819159+RioluMeltan@users.noreply.github.com>
2025-10-14 11:14:02 +0000

    Caching to fix speed issues

--- streamlit_app.py
+++ streamlit_app.py
@@ -26,8 +26,13 @@ if streamlit.sidebar.button('Add to Watchlist'):
         streamlit.sidebar.success(f'You have added {ticker.upper()} to your watchlist!')
 
 streamlit.sidebar.subheader('Your Watchlist')
+
+@streamlit.cache_data
+def get_change_data(ticker): 
+    return yfinance.download(ticker, start = datetime.datetime.now() - datetime.timedelta(days = 1), end = datetime.datetime.now(), interval = '1d', progress = False, auto_adjust = True)
+
 for stock in streamlit.session_state.tracked_stocks: 
-    close_diff = yfinance.download(stock, start = datetime.datetime.now() - datetime.timedelta(days = 1), end = datetime.datetime.now(), interval = '1d', progress = False, auto_adjust = True)
+    close_diff = get_change_data(stock)
     if not close_diff.empty: 
         diff = (float(close_diff['Open'].iloc[0]) - float(close_diff['Close'].iloc[-1])) / float(close_diff['Open'].iloc[0])
         color = 'green' if diff >= 0 else 'red'
@@ -37,9 +42,14 @@ for stock in streamlit.session_state.tracked_stocks:
         streamlit.sidebar.markdown(f"<div style = 'border: 1px solid #ccc; padding: 10px; border-radius: 5px; margin-bottom: 10px;'><strong>{stock}</strong><br><span style = 'color: gray;'>Unavailable change amount</span></div>", unsafe_allow_html = True)
 
 streamlit.title('Your Watchlist Performance')
+
+@streamlit.cache_data
+def get_long_data(ticker): 
+    return yfinance.download(stock, start = datetime.datetime.now() - datetime.timedelta(days = selections[selected_range]), end = datetime.datetime.now(), interval = '1d', progress = False, auto_adjust = True)
+
 for stock in streamlit.session_state.tracked_stocks: 
     streamlit.subheader(f'{stock} - Historical Data')
     selections = {'Previous 5 Days': 5, 'Previous Month': 30, 'Previous 6 Months': 180, 'Previous Year': 365}
     selected_range = streamlit.radio('Select time range: ', list(selections.keys()), index = 3, horizontal = True, key = f'range_{stock}')
-    data = yfinance.download(stock, start = datetime.datetime.now() - datetime.timedelta(days = selections[selected_range]), end = datetime.datetime.now(), interval = '1d', progress = False, auto_adjust = True)
+    data = get_long_data(stock)
     streamlit.line_chart(data['Close'])
