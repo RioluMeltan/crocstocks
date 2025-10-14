b0e0fe11be9e0f2a73e9ae0a1f91c9b936907e4e
RioluMeltan <78819159+RioluMeltan@users.noreply.github.com>
2025-10-14 04:00:01 +0000

    Small changes

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,8 +8,17 @@ import time
 import GoogleNews
 from nltk.sentiment.vader import SentimentIntensityAnalyzer
 
+if 'tracked_stocks' not in streamlit.session_state:
+    streamlit.session_state.tracked_stocks = []
+
 streamlit.set_page_config(page_title = 'CrocStocks Stock Predictor', layout = 'wide')
-streamlit.sidebar.header('Selected Stocks')
+streamlit.sidebar.header('Add Stocks to Watchlist')
 ticker = streamlit.sidebar.text_input('Enter Stock Ticker', value = 'AAPL')
 start_date = streamlit.sidebar.date_input('Start Date', value = datetime.datetime.now() - datetime.timedelta(days = 365))
 end_date = streamlit.sidebar.date_input('End Date', value = datetime.datetime.now())
+if streamlit.sidebar.button('Add to Watchlist'):
+    if ticker.upper() not in streamlit.session_state.tracked_stocks:
+        streamlit.session_state.tracked_stocks.append(ticker.upper())
+        streamlit.sidebar.success(f'You have added {ticker.upper()} to your watchlist!')
+    elif ticker.upper() in streamlit.session_state.tracked_stocks:
+        streamlit.sidebar.warning(f"{ticker.upper()} is already in your watchlist.")
