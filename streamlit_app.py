99bead77d4c6cdab8c4d6ff6c8b5006089c25594
RioluMeltan <78819159+RioluMeltan@users.noreply.github.com>
2025-10-14 03:47:08 +0000

    Start of a basic framework, also dependencies

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,15 @@
-import streamlit as st
+import streamlit
+import yfinance
+import datetime
+import sklearn
+import tensorflow
+import numpy
+import time
+import GoogleNews
+from nltk.sentiment.vader import SentimentIntensityAnalyzer
 
-st.title("🎈 My new app")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
-)
+streamlit.set_page_config(page_title = 'CrocStocks Stock Predictor', layout = 'wide')
+streamlit.sidebar.header('Selected Stocks')
+ticker = streamlit.sidebar.text_input('Enter Stock Ticker', value = 'AAPL')
+start_date = streamlit.sidebar.date_input('Start Date', value = datetime.datetime.now() - datetime.timedelta(days = 365))
+end_date = streamlit.sidebar.date_input('End Date', value = datetime.datetime.now())
