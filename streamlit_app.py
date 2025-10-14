feb768f9a2c0251b84a8f03e44a860792b6a91fb
RioluMeltan <78819159+RioluMeltan@users.noreply.github.com>
2025-10-14 07:29:17 +0000

    More attempts at bug fixing

--- streamlit_app.py
+++ streamlit_app.py
@@ -44,6 +44,9 @@ for stock in streamlit.session_state.tracked_stocks:
     data = yfinance.download(stock, start = datetime.datetime.now() - datetime.timedelta(days = selections[selected_range]), end = datetime.datetime.now(), interval = '1d', progress = False, auto_adjust = True)
     
     fig = plotly.graph_objs.Figure()
-    fig.add_trace(plotly.graph_objs.Scatter(x = data.index, y = data['Close'].values, mode = 'lines', name = stock, line = dict(color = 'blue', width = 2, dash = 'solid')))
-    fig.update_layout(title = f'{stock} Historical Closing Prices in the {selected_range}', xaxis_title = 'Date', yaxis_title = "Price (USD)", template = 'plotly_dark', showlegend = True, height = 400)
+    fig.add_trace(plotly.graph_objs.Scatter(x = data.index, y = data['Close'], mode = 'lines', name = stock, line = dict(color = 'blue', width = 2, dash = 'solid')))
+    y_min = data['Close'].dropna().min()
+    y_max = data['Close'].dropna().max()
+    padding = (data['Close'].dropna().max() - data['Close'].dropna().min()) * 0.05 or 1
+    fig.update_layout(title = f'{stock} Historical Closing Prices in the {selected_range}', xaxis_title = 'Date', yaxis_title = "Price (USD)", yaxis = dict(range = [y_min - padding, y_max + padding]), template = 'plotly_dark', showlegend = True, height = 400)
     streamlit.plotly_chart(fig, use_container_width = True)
