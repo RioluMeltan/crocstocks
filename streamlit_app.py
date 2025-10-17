9edad1ea857493da10acc562cac1d48b3338ecfc
RioluMeltan <78819159+RioluMeltan@users.noreply.github.com>
2025-10-17 14:08:38 +0000

    Exception handling

--- streamlit_app.py
+++ streamlit_app.py
@@ -69,5 +69,8 @@ for stock in streamlit.session_state.tracked_stocks:
     streamlit.subheader(f'{stock} - Historical Data')
     selections = {'Previous 5 Days': 5, 'Previous Month': 30, 'Previous 6 Months': 180, 'Previous Year': 365, 'Previous 5 Years': 1825, 'All Time': 99999}
     selected_range = streamlit.radio('Select time range: ', list(selections.keys()), index = 5, horizontal = True, key = f'range_{stock}')
-    data = get_long_data(stock, selections[selected_range])
-    streamlit.line_chart(data['Close'])
+    try: 
+        data = get_long_data(stock, selections[selected_range])
+        streamlit.line_chart(data['Close'])
+    except Exception as ex: 
+        streamlit.warning(ex)
