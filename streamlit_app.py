5686d056191e4db2d4c46d8f475e8a1d7b75706b
RioluMeltan <78819159+RioluMeltan@users.noreply.github.com>
2025-10-23 23:54:24 +0000

    Initialization forgot

--- streamlit_app.py
+++ streamlit_app.py
@@ -25,8 +25,8 @@ class ProgressCallback(tensorflow.keras.callbacks.Callback):
 if 'tracked_stocks' not in streamlit.session_state:
     streamlit.session_state.tracked_stocks = []
 
-if 'quick_open' not in streamlit.session_state: 
-    streamlit.session_state.quick_open = False
+if 'quick_rerun' not in streamlit.session_state: 
+    streamlit.session_state.quick_rerun = True
 
 streamlit.set_page_config(page_title = 'CrocStocks Stock Predictor', layout = 'wide')
 streamlit.sidebar.header('Add Stocks to Watchlist')
