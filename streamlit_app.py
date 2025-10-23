a774ec3fdfa77224e4cdb6abb1416049e39984cc
RioluMeltan <78819159+RioluMeltan@users.noreply.github.com>
2025-10-23 23:34:48 +0000

    Moving variables around

--- streamlit_app.py
+++ streamlit_app.py
@@ -170,6 +170,7 @@ def historical_analysis(symbol, _prog_bar, total, curr):
 
 @streamlit.dialog('Quick Analysis Results', width = 'medium', dismissible = False)
 def quick(f_true, s_true, h_true, stock): 
+    streamlit.session_state.quick_open = True
     progress_struct = streamlit.empty()
     progress = progress_struct.progress(0)
     if f_true: 
@@ -188,7 +189,7 @@ def quick(f_true, s_true, h_true, stock):
     if h_true: 
         streamlit.code(f'{stock} Projected Next Day Close: US${h_results:.4f}')
     if streamlit.button('Close'): 
-        streamlit.rerun(scope = 'app')
+        streamlit.rerun()
 
 for stock in streamlit.session_state.tracked_stocks: 
     streamlit.subheader(f'{stock} - Historical Data')
@@ -208,7 +209,6 @@ for stock in streamlit.session_state.tracked_stocks:
                 streamlit.markdown("<button disabled style = 'opacity:0.6;'>Comprehensive Analysis</button>", unsafe_allow_html = True)
             else: 
                 if streamlit.button('Quick Analysis', key = 'quick' + stock) and not streamlit.session_state.quick_open: 
-                    streamlit.session_state.quick_open = True
                     quick(fundamental_check, sentiment_check, historical_check, stock)
                 elif streamlit.session_state.quick_open: 
                     streamlit.session_state.quick_open = False
