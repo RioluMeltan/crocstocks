6967cf7c62731e21d94b211b511bccbea4607a6b
RioluMeltan <78819159+RioluMeltan@users.noreply.github.com>
2025-10-23 16:07:44 +0000

    Removed rerun

--- streamlit_app.py
+++ streamlit_app.py
@@ -184,8 +184,6 @@ def quick(f_true, s_true, h_true, stock):
         streamlit.code(f'{stock} Market Sentiment: {s_results:.4f}')
     if h_true: 
         streamlit.code(f'{stock} Projected Next Day Close: US${h_results:.4f}')
-    if streamlit.button('Close'): 
-        streamlit.rerun()
 
 for stock in streamlit.session_state.tracked_stocks: 
     streamlit.subheader(f'{stock} - Historical Data')
