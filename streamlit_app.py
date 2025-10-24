ac3f491d0a062ce81dbfd21c7e2be9089f94e3e5
RioluMeltan <78819159+RioluMeltan@users.noreply.github.com>
2025-10-24 00:09:25 +0000

    Revert

--- streamlit_app.py
+++ streamlit_app.py
@@ -210,10 +210,9 @@ for stock in streamlit.session_state.tracked_stocks:
                 streamlit.markdown("<button disabled style = 'opacity:0.6;'>Quick Analysis</button>", unsafe_allow_html = True)
                 streamlit.markdown("<button disabled style = 'opacity:0.6;'>Comprehensive Analysis</button>", unsafe_allow_html = True)
             else: 
-                if not streamlit.session_state.quick_open: 
-                    if streamlit.button('Quick Analysis', key = 'quick' + stock): 
-                        streamlit.session_state.quick_open = True
-                        quick(fundamental_check, sentiment_check, historical_check, stock)
+                if streamlit.button('Quick Analysis', key = 'quick' + stock) and not streamlit.session_state.quick_open: 
+                    streamlit.session_state.quick_open = True
+                    quick(fundamental_check, sentiment_check, historical_check, stock)
                 elif streamlit.session_state.quick_open: 
                     streamlit.session_state.quick_open = False
                 if streamlit.button('Comprehensive Analysis', key = 'comprehensive' + stock): 
