3eb376111c1ca8efe0f5e21211b35dbf7a24eec5
RioluMeltan <78819159+RioluMeltan@users.noreply.github.com>
2025-10-25 07:46:49 +0000

    Miscalculated

--- streamlit_app.py
+++ streamlit_app.py
@@ -128,8 +128,8 @@ def quick_all():
             coeff += int(100 / (3 * len(streamlit.session_state.tracked_stocks)))
             h_results = historical_analysis(stock, progress, 3 * len(streamlit.session_state.tracked_stocks), coeff)
             progress_struct.empty()
-            coeff = int(100 / (len(streamlit.session_state.tracked_stocks) - iter))
             iter += 1
+            coeff = int((100 / len(streamlit.session_state.tracked_stocks)) * iter)
             streamlit.code(f'{stock} Current Day Close: {get_change_data(stock)['Close'].values[-1][0]:.2f} USD\n{stock} Fundamentals: {f_results[0]:.4f}%\n{stock} Market Sentiment: {(s_results * 100):.4f}%\n{stock} Projected Next Day Close: {h_results:.2f} USD', language = None)
         progress.progress(100)
         streamlit.session_state.quick_rerun = True
